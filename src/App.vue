90ba8f32de4aef46761eb2d613aeb553556f3b47
Prakanksha <nagpalprakankshabvpy@gmail.com>
2024-09-14 02:51:53 +0530

    4

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,8 @@
 <template>
-  <v-app style="background-color: #FCE4EC;">
+  <v-app style="background-color: #FCE4EC; min-height: 100vh; display: flex; flex-direction: column;">
     <v-container>
       <div class="text-center">
-        <div class="text-h5 text-secondary mb-n3" style="font-family: 'Pacifico', cursive;">
+        <div class="text-h5 mb-n3" style="font-family: 'Pacifico', cursive; color: black;">
           Hi! I am Prakanksha and this is all me.
         </div>
       </div>
@@ -20,36 +20,30 @@
         ></v-img>
       </v-card>
     </v-container>
-    <v-speed-dial
-      location="right center"
-      transition="fade-transition"
-      style="width: 500px;"
-    >
-      <template v-slot:activator="{ props: activatorProps }">
-        <v-fab
-          v-bind="activatorProps"
-          size="small"
-          icon="mdi-account-box"
-          rounded="circle"
-          color="white"
-        ></v-fab>
-      </template>
-
-      <v-btn key="1" rounded="circle" icon="mdi-github" color="white"  @click="openLink('https://github.com/Prakanksh')"></v-btn>
-      <v-btn key="2" rounded="circle" icon="mdi-linkedin" color="white" @click="openLink('https://www.linkedin.com/in/prakanksha-nagpal-b6a90021b')"></v-btn>
-      <v-btn key="3" rounded="circle" icon="mdi-gmail" color="white" @click="openLink('https://mail.google.com/mail/?view=cm&fs=1&to=nagpalprakankshabvpy@gmail.com')"></v-btn>
-      <v-btn key="4" rounded="circle" icon="mdi-instagram" color="white" @click="openLink('https://www.instagram.com/_pra_amodini_')"></v-btn>
-    </v-speed-dial>
+    
+    <v-spacer></v-spacer>
+    <v-divider></v-divider>
+    <v-container class="d-flex align-center justify-center" style="padding-bottom: 20px;">
+      <span class="text-h6 mr-3" style="font-family: 'Pacifico', cursive;">Reach me out:</span>
+      <div class="d-flex justify-center">
+        <v-btn key="1" rounded="circle" icon="mdi-github" color="white" class="mx-2" @click="openLink('https://github.com/Prakanksh')"></v-btn>
+        <v-btn key="2" rounded="circle" icon="mdi-linkedin" color="white" class="mx-2" @click="openLink('https://www.linkedin.com/in/prakanksha-nagpal-b6a90021b')"></v-btn>
+        <v-btn key="3" rounded="circle" icon="mdi-gmail" color="white" class="mx-2" @click="openLink('https://mail.google.com/mail/?view=cm&fs=1&to=nagpalprakankshabvpy@gmail.com')"></v-btn>
+        <v-btn key="4" rounded="circle" icon="mdi-instagram" color="white" class="mx-2" @click="openLink('https://www.instagram.com/_pra_amodini_')"></v-btn>
+      </div>
+    </v-container>
   </v-app>
 </template>
 
 <script>
 import screenshotImage from '@/assets/Screenshot.jpg';
+import Vine from '@/assets/Vine.jpg';
 
 export default {
   data() {
     return {
-      screenshot: screenshotImage
+      screenshot: screenshotImage,
+      Vine : Vine
     };
   },
   methods: {
