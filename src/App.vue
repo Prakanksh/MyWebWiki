4d5573e4449736b3eaa96d66b70887d933070ba6
Prakanksha <nagpalprakankshabvpy@gmail.com>
2024-09-20 11:44:42 +0530

    footer added as component

--- src/App.vue
+++ src/App.vue
@@ -86,31 +86,23 @@
     <!-- Render routed pages here -->
     <router-view></router-view>
 
-    <v-spacer></v-spacer>
-    <v-divider></v-divider>
-    <v-container class="d-flex align-center justify-center" style="padding-bottom: 20px;">
-      <span class="text-h6 mr-3" style="font-family: 'Pacifico', cursive;">Reach me out:</span>
-      <div class="d-flex justify-center">
-        <v-btn key="1" rounded="circle" icon="mdi-github" color="white" class="mx-2"
-          @click="openLink('https://github.com/Prakanksh')"></v-btn>
-        <v-btn key="2" rounded="circle" icon="mdi-linkedin" color="white" class="mx-2"
-          @click="openLink('https://www.linkedin.com/in/prakanksha-nagpal-b6a90021b')"></v-btn>
-        <v-btn key="3" rounded="circle" icon="mdi-gmail" color="white" class="mx-2"
-          @click="openLink('https://mail.google.com/mail/?view=cm&fs=1&to=nagpalprakankshabvpy@gmail.com')"></v-btn>
-        <v-btn key="4" rounded="circle" icon="mdi-instagram" color="white" class="mx-2"
-          @click="openLink('https://www.instagram.com/_pra_amodini_')"></v-btn>
-      </div>
-    </v-container>
+    <Footer />
   </v-app>
 </template>
 
 <script>
 import screenshotImage from '@/assets/Screenshot.jpg';
+import Footer from '@/components/Footer.vue';
 
 export default {
+  components: {
+    Footer
+  },
+
   data() {
     return {
       screenshot: screenshotImage,
+      Footer: Footer,
       showAboutCard: false,
       showImageCard: false,
       showEducationCard: false,
